a43cec7c0aaa181735f5d74ff468cdd1c9685634
Keshav Sharma <keshav2743@gmail.com>
2024-03-17 18:54:49 +0530

    Arrays, dict and tuples

---
+++ dict.py
@@ -0,0 +1,11 @@
+# Dictionary - Maps
+pizzas = {
+    "cheese" : 9,
+    "vegetable": 12,
+    "pepperoni": 23
+}
+print(pizzas["pepperoni"])
+
+# Transforming dictonary to lists
+for pie, price in pizzas.items():
+    print(price)
--- tuple.py
+++ tuple.py
@@ -4,5 +4,14 @@ students = [
     ("Hemp", 54)
 ]
 
+# Just like in C, printf("%d", i) we use "".format(...params)
+
+
+# If you want leave empty curly braces it will automatically fill the params from left to right
 for name, roll in students:
-    print("Name:  {0} and rollno: {1}".format(name,roll))
+    print("Name:  {} and rollno: {}".format(name,roll))
+
+# But if you want to print in certain order like I want to print roll first then name
+    
+for name, roll in students:
+    print("Roll: {1} and Name: {0}".format(name, roll))
